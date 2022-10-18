5db846e7c93feeb6dc8dd995eb92245924aeef05
Angel <75736079+bryanind195@users.noreply.github.com>
2022-10-18 16:56:56 -0500

    se realiza el lsitado de productos 44:26 min

--- vivamex/components/ProductCard.jsx
+++ vivamex/components/ProductCard.jsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import styles from "../styles/ProductCard.module.css";
 
 const ProductCard = () => {
   return (
     <div className={styles.container} >
-      Productos
+      <Image src="/img/AGUA FRESCA MOLECULAR.jpg" alt="500" width="500" height="500" />
+      <h1 className={styles.title} > Maruloco </h1>
+      <span className={styles.price}> $3.50</span>
+            <p className={styles.desc} >
+            Lorem Ipsum es simplemente un texto ficticio de la industria 
+            </p>
     </div>
   )
 }
